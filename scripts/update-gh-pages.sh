87d0c48a1995ae13a398afc1d833bc0d9d1329c8
Dave Moten <davidmoten@gmail.com>
2022-02-15 10:19:08 +1100

    gh-pages dev

--- scripts/update-gh-pages.sh
+++ scripts/update-gh-pages.sh
@@ -6,6 +6,6 @@ cp ../target/index.html /tmp
 git checkout gh-pages
 cp /tmp/index.html ..
 git add ..
-git commit -am "update index.html"
-git push -u origin gh-pages || git checkout master
+git commit -am "update index.html" 
+git push -u origin gh-pages 
 git checkout master
