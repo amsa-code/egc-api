972fcaf4dac5101af55b66bf49b566364d5cc7f1
Dave Moten <davidmoten@gmail.com>
2022-02-15 10:00:09 +1100

    update index.html

---
+++ scripts/update-gh-pages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+set -x
+./create-docs.sh
+cp ../target/index.html /tmp
+git checkout gh-pages
+cp /tmp/index.html ..
+git add ..
+git commit -am "update index.html"
+git push
+git checkout master
